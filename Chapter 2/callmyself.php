dad2b79f8fd2e86354da42ce6aa15bf7e5a2e63e
Rockabit <67980604+Rockabit@users.noreply.github.com>
2023-03-18 18:09:29 +0100

    Update callmyself.php
    Added body opening tag

--- Chapter 2/callmyself.php
+++ Chapter 2/callmyself.php
@@ -6,7 +6,7 @@ print "<h1> Hello World </h1>";
 else
 {
 print "<html><head><title>PHP Example</title></head>";
-print "<form method='post' action='e26callmyself.php'>";
+print "<body><form method='post' action='e26callmyself.php'>";
 print "<input type='submit' id='submitbutton' name='submitbutton' value='Find Hello World!'/>";
 print "</form>";
 print "</body></html>";
